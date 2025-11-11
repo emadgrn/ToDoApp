8b50b9a13343666eeec2a2cd125a6d2eeff0f68e
Emad Gerami <emad.grnd@gmail.com>
2025-11-11 17:24:19 +0330

    Add project files.

---
+++ src/01.Domain/Core/ToDoApp.Domain.Core/_common/Entities/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Domain.Core._common.Entities
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+
+        public static Result<T> Success(string message, T? data = default) =>
+            new() { IsSuccess = true, Message = message, Data = data };
+
+        public static Result<T> Failure(string message, T? data = default) =>
+            new() { IsSuccess = false, Message = message, Data = data };
+    }
+}
+
