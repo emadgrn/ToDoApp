3cc04ba5d87e255652a2954b8c2898cdb4ca590a
Emad Gerami <emad.grnd@gmail.com>
2025-11-12 12:28:29 +0330

    add initial User - Category - ToDo

---
+++ src/01.Domain/Core/ToDoApp.Domain.Core/ToDoAgg/Enums/StatusEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoApp.Domain.Core.ToDoAgg.Enums;
+
+public enum StatusEnum
+{
+    Done=1,
+    Pending=2,
+    Overdue=3
+}
---
+++ src/01.Domain/Core/ToDoApp.Domain.Core/UserAgg/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Core.ToDoAgg.Entities;
+
+namespace ToDoApp.Domain.Core.UserAgg.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public List<ToDo> ToDos { get; set; } = [];
+
+    }
+}
