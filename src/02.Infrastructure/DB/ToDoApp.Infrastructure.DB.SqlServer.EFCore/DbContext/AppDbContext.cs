30dc5266254d63dd1bae658b4acabdd4bf006a21
Emad Gerami <emad.grnd@gmail.com>
2025-11-12 19:13:29 +0330

    add db context

---
+++ src/02.Infrastructure/DB/ToDoApp.Infrastructure.DB.SqlServer.EFCore/DbContext/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Core.CategoryAgg.Entities;
+using ToDoApp.Domain.Core.ToDoAgg.Entities;
+using ToDoApp.Domain.Core.UserAgg.Entities;
+using ToDoApp.Infrastructure.DB.SqlServer.EFCore.Configurations;
+
+namespace ToDoApp.Infrastructure.DB.SqlServer.EFCore.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ToDo> Todos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new ToDoConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
