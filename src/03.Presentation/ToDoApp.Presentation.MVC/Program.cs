c066eba14ae7180137c605e681be45cba7003bf4
Emad Gerami <emad.grnd@gmail.com>
2025-11-13 22:27:28 +0330

    add mvc

---
+++ src/03.Presentation/ToDoApp.Presentation.MVC/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Domain.Core.CategoryAgg.Contracts;
+using ToDoApp.Domain.Core.ToDoAgg.Contracts;
+using ToDoApp.Domain.Core.UserAgg.Contracts;
+using ToDoApp.Domain.Services.User;
+using ToDoApp.Infrastructure.DB.SqlServer.EFCore.DbContext;
+using ToDoApp.Infrastructure.Repositories.EFCore.CategoryAgg;
+using ToDoApp.Infrastructure.Repositories.EFCore.ToDoAgg;
+using ToDoApp.Infrastructure.Repositories.EFCore.UserAgg;
+
+namespace ToDoApp.Presentation.MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IUserService, UserService>();
+
+            builder.Services.AddScoped<IToDoRepository, ToDoRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
