157fa8c749f13512a31d9d32a8af01115434c130
Emad Gerami <emad.grnd@gmail.com>
2025-11-13 22:30:28 +0330

    add user and todo service

---
+++ src/01.Domain/Services/ToDoApp.Domain.Services/ToDo/ToDoService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Core._common.Entities;
+using ToDoApp.Domain.Core.ToDoAgg.Contracts;
+using ToDoApp.Domain.Core.ToDoAgg.DTOs;
+using ToDoApp.Domain.Core.ToDoAgg.Enums;
+using ToDoApp.Domain.Core.UserAgg.DTOs;
+
+namespace ToDoApp.Domain.Services.ToDo
+{
+    public class ToDoService(IToDoRepository toDoRepo):IToDoService
+    {
+        public Result<bool> CreateToDo(CreateToDoDto model)
+        {
+            try
+            {
+                toDoRepo.Create(model);
+                return Result<bool>.Success("کار با موفقیت ذخیره شد.");
+            }
+            catch
+            {
+                return Result<bool>.Failure("ایجاد کار با خطا روبرو شد.");
+            }
+        }
+
+        public Result<GetToDoDto> GetToDoById(int id)
+        {
+            var toDoDto = toDoRepo.GetById(id);
+
+            if (toDoDto is not null)
+            {
+                return Result<GetToDoDto>.Success("", toDoDto);
+            }
+            else
+            {
+                return Result<GetToDoDto>.Failure($"کار با این آیدی {id} وجود ندارد");
+            }
+        }
+
+        public List<GetToDoDto> GetAllToDosByUserId(int userId)
+        {
+            return toDoRepo.GetAllToDosByUserId(userId);
+        }
+
+        public StatusEnum? GetStatus(int toDoId)
+        {
+            return toDoRepo.GetStatusById(toDoId);
+        }
+
+        public void MarkAsDone(int toDoId)
+        {
+             toDoRepo.SetDone(toDoId);
+        }
+
+        public void MarkAsPending(int toDoId)
+        {
+             toDoRepo.SetPending(toDoId);
+        }
+
+        public void MarkAsOverDue(int toDoId)
+        {
+             toDoRepo.SetOverdue(toDoId);
+        }
+    }
+}
---
+++ src/01.Domain/Services/ToDoApp.Domain.Services/User/UserService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Core._common.Entities;
+using ToDoApp.Domain.Core.UserAgg.Contracts;
+using ToDoApp.Domain.Core.UserAgg.DTOs;
+
+namespace ToDoApp.Domain.Services.User
+{
+    public class UserService(IUserRepository userRepo) : IUserService
+    {
+
+
+        public Result<GetUserSummaryDto> Login(string username, string password)
+        {
+            var loginData = userRepo.Login(username, password);
+
+            if (loginData is not null)
+            {
+                return Result<GetUserSummaryDto>.Success("لاگین با موفقیت انجام شد.", loginData);
+
+            }
+            else
+            {
+                return Result<GetUserSummaryDto>.Failure("نام کاربری یا کلمه عبور اشتباه می باشد.");
+            }
+        }
+
+        public Result<GetUserSummaryDto> Register(CreateUserDto model)
+        {
+            var usernameExist = userRepo.UsernameExists(model.Username);
+
+            if (usernameExist)
+            {
+                return Result<GetUserSummaryDto>.Failure("کاربر با این نام کاربری موجود می باشد.");
+            }
+
+            var registerData = userRepo.Register(model);
+
+            if (registerData is not null)
+            {
+                return Result<GetUserSummaryDto>.Success("ثبت نام با موفقیت انجام شد.", registerData);
+
+            }
+            else
+            {
+                return Result<GetUserSummaryDto>.Failure("ثبت نام با مشکل مواجه شد.");
+            }
+        }
+
+        public Result<GetUserDto> GetUserById(int id)
+        {
+            var userDto = userRepo.GetById(id);
+
+            if (userDto is not null)
+            {
+                return Result<GetUserDto>.Success("", userDto);
+            }
+            else
+            {
+                return Result<GetUserDto>.Failure($"کاربر با این آیدی {id} وجود ندارد");
+            }
+        }
+
+        public UpdateUserDto? GetUpdateUserDetails(int userId)
+        {
+            return userRepo.GetUpdateUserDetailsById(userId);
+        }
+
+        public Result<bool> Update(int userId, UpdateUserDto model)
+        {
+            var result = userRepo.Update(userId, model);
+
+            if (result)
+            {
+                return Result<bool>.Success("اطلاعات کاربر با موفقیت به‌روزرسانی شد.");
+            }
+            else
+            {
+                return Result<bool>.Failure("به‌روزرسانی اطلاعات کاربر با خطا مواجه شد.");
+            }
+        }
+
+        public bool DeleteUser(int userId)
+        {
+            return userRepo.DeleteById(userId);
+        }
+    }
+}
