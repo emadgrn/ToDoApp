228aa9f9ad2b5c0cc0a868fad43121419a8ba6bc
Emad Gerami <emad.grnd@gmail.com>
2025-11-13 23:36:27 +0330

    add brief description property

--- src/01.Domain/Core/ToDoApp.Domain.Core/CategoryAgg/Entities/Category.cs
+++ src/01.Domain/Core/ToDoApp.Domain.Core/CategoryAgg/Entities/Category.cs
@@ -4,6 +4,7 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string? BriefDescription { get; set; }
     public string? ImageUrl { get; set; }
 
 }
