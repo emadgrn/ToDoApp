286d8bd4faebeac8d0e50af21da0adb2c4fb3cef
Emad Gerami <emad.grnd@gmail.com>
2025-11-13 23:38:12 +0330

    add in memory db and online user

---
+++ src/03.Presentation/ToDoApp.Presentation.MVC/Database/InMemoryDatabase.cs
@@ -0,0 +1,9 @@
+﻿using ToDoApp.Presentation.MVC.Models;
+
+namespace ToDoApp.Presentation.MVC.Database
+{
+    public static class InMemoryDatabase
+    {
+        public static OnlineUser? OnlineUser { get; set; }
+    }
+}
---
+++ src/03.Presentation/ToDoApp.Presentation.MVC/Models/OnlineUser.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApp.Presentation.MVC.Models
+{
+    public class OnlineUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+    }
+}
