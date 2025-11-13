eac6aa68a452f5f254cfd31cb37640d0ffde9e3f
Emad Gerami <emad.grnd@gmail.com>
2025-11-13 10:15:01 +0330

    add File service

---
+++ src/02.Infrastructure/File/ToDoApp.Infrastructure.FileService/Contracts/IFileService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+
+namespace ToDoApp.Infrastructure.FileService.Contracts
+{
+    public interface IFileService
+    {
+        public string Upload(IFormFile file, string folder);
+        public void Delete(string fileName);
+    }
+}
---
+++ src/02.Infrastructure/File/ToDoApp.Infrastructure.FileService/Services/FileService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ToDoApp.Infrastructure.FileService.Contracts;
+
+namespace ToDoApp.Infrastructure.FileService.Services
+{
+    public class FileService : IFileService
+    {
+        public void Delete(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+                return;
+
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", fileName);
+
+            if (File.Exists(fullPath))
+            {
+                File.Delete(fullPath);
+            }
+        }
+
+
+        public string Upload(IFormFile file, string folder)
+        {
+
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Files", folder);
+
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return $"{Path.Combine("Files", folder, uniqueFileName)}";
+        }
+    }
+}
