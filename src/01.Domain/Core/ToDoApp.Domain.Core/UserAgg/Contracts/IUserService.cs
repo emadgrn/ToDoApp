b74db382a70fbcb89e3a1591198c01eb2ef63d29
Emad Gerami <emad.grnd@gmail.com>
2025-11-13 22:29:13 +0330

    add contracts for useragg

---
+++ src/01.Domain/Core/ToDoApp.Domain.Core/UserAgg/Contracts/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Core._common.Entities;
+using ToDoApp.Domain.Core.UserAgg.DTOs;
+
+namespace ToDoApp.Domain.Core.UserAgg.Contracts
+{
+    public interface IUserService
+    {
+        Result<GetUserSummaryDto> Login(string mobile, string password);
+        Result<GetUserSummaryDto> Register(CreateUserDto model);
+        Result<GetUserDto> GetUserById(int id);
+        UpdateUserDto? GetUpdateUserDetails(int userId);
+        Result<bool> Update(int userId, UpdateUserDto model);
+        bool DeleteUser(int userId);
+    }
+}
