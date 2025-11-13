3d167d2963a6451d450647d48ed9180380056c1d
Emad Gerami <emad.grnd@gmail.com>
2025-11-13 22:26:16 +0330

    add LoginViewModel and RegisterViewModel

---
+++ src/03.Presentation/ToDoApp.Presentation.MVC/Models/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApp.Presentation.MVC.Models
+{
+    public class RegisterViewModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
